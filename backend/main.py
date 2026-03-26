766cd0989258cdf14c8dff8ace09faa9e0301b0b
Naina-Shukla <shuklanaina1111@gmail.com>
2026-03-26 21:55:08 +0530

    changed path

--- backend/main.py
+++ backend/main.py
@@ -22,7 +22,12 @@ DB_PATH = Path(r"D:\DOCUMENTSS\graph-o2c\backend\data.db")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000", "*"],
+    allow_origins=[
+        "http://localhost:3000",
+        "http://127.0.0.1:3000",
+        "https://graph-o2c-fhbhvc30y-shuklanaina1111-7152s-projects.vercel.app",
+        "*"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
